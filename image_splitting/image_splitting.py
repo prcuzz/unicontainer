06913bbc5099121a1d0a04b699005849caa4eb49
prcuzz <403124323@qq.com>
2023-09-29 17:45:16 +0800

    update image splitting

--- image_splitting/image_splitting.py
+++ image_splitting/image_splitting.py
@@ -1,14 +1,31 @@
 def analyze_line(line):
-	
+    if "resumed>)" in line:
+        return
+    else:
+        pid = line.split(' ', 1)[0]
+        rest_of_the_line = line.split(' ', 1)[1]
+        syscall = rest_of_the_line.split('(', 1)[0]
+        return pid, syscall
 
-def image_splitting():
-	strace_output = ""
-	with open(strace_output,'r',encoding = 'utf-8') as f:
-		line = f.readline()
-		while(line):
-			analyze_line(line)
-			line = f.readline()
-		
+
+def image_splitting(strace_output):
+    file_syscall=["access","open"]
+
+    with open(strace_output, 'r', encoding='utf-8') as f:
+        line = f.readline()
+        while (line):
+            analyze_line(line)
+            if(syscall is exec):
+                create_a_new_file_group()
+            elif(syscall in file_syscall):
+                add_this_file_into_corresponding_file_group()
+            elif(syscall is clone):
+
+            else:
+                pass
+            line = f.readline()
+
 
 if __name__ == '__main__':
-	image_splitting()
+    strace_output = "./imagemagick_strace_output"
+    image_splitting(strace_output)
